db8dbc2e442b5adb65b2239681ded6e3dc4c6085
Thien <cttam.ng@gmail.com>
2025-09-15 21:28:33 +0700

    Them xu ly loi api

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routers/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404. "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.stautsCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book appilication."});
 });
