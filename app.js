ca29d015f2d1b49744b3c18fb771363368d079cf
Thien <cttam.ng@gmail.com>
2025-09-15 21:29:36 +0700

    Them xu ly loi api

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
-    return next(new ApiError(404. "Resource not found"));
+    return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
     return res.status(error.stautsCode || 500).json({
