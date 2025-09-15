357919ce7c615dbb4de97e816b1caaa0f7525ff6
Thien <cttam.ng@gmail.com>
2025-09-15 22:23:07 +0700

    Sua Loi 404

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book appilication."});
+});
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
-    return res.status(error.stautsCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book appilication."});
-});
+
 
 module.exports = app;
